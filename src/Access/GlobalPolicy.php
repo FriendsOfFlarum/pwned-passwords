e908ab512757ec53ea352f8e06a617d85b3040eb
Tristian Kelly <ordinaryjellyfish@protonmail.com>
2020-01-22 00:58:26 +0000

    Apply fixes from StyleCI

--- src/Access/GlobalPolicy.php
+++ src/Access/GlobalPolicy.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of fof/pwned-passwords.
+ *
+ * Copyright (c) 2019 FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace FoF\PwnedPasswords\Access;
 
 use Flarum\Event\GetPermission;
