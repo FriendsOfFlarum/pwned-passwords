9bdcc893cd752f6c5a977ccdca26e5d8886fe02d
David Sevilla Martin <me@dsevilla.dev>
2026-04-05 21:38:10 -0400

    Remove commented JS code

--- js/src/admin/extend.ts
+++ js/src/admin/extend.ts
@@ -1,32 +1,16 @@
-// .for('fof-pwned-passwords')
-//   .registerSetting({
-//     label: app.translator.trans('fof-pwned-passwords.admin.enableLoginCheck'),
-//     setting: 'fof-pwned-passwords.enableLoginCheck',
-//     type: 'boolean',
-//   })
-//   .registerSetting({
-//     label: app.translator.trans('fof-pwned-passwords.admin.enableAdminRevoke'),
-//     setting: 'fof-pwned-passwords.revokeAdminAccess',
-//     type: 'boolean',
-//   });
-
 import Extend from 'flarum/common/extenders';
 import app from 'flarum/admin/app';
 
 export default [
   new Extend.Admin()
-    .setting(
-      () => ({
-        setting: 'fof-pwned-passwords.enableLoginCheck',
-        type: 'boolean',
-        label: app.translator.trans('fof-pwned-passwords.admin.enableLoginCheck', {}, true),
-      })
-    )
-    .setting(
-      () => ({
-        setting: 'fof-pwned-passwords.revokeAdminAccess',
-        type: 'boolean',
-        label: app.translator.trans('fof-pwned-passwords.admin.enableAdminRevoke', {}, true),
-      })
-    )
-]
+    .setting(() => ({
+      setting: 'fof-pwned-passwords.enableLoginCheck',
+      type: 'boolean',
+      label: app.translator.trans('fof-pwned-passwords.admin.enableLoginCheck', {}, true),
+    }))
+    .setting(() => ({
+      setting: 'fof-pwned-passwords.revokeAdminAccess',
+      type: 'boolean',
+      label: app.translator.trans('fof-pwned-passwords.admin.enableAdminRevoke', {}, true),
+    })),
+];
